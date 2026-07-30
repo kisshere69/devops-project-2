fbe7296b19840d66fe3059d0d6af3dfbf0eea9b1
Nikita <abc@gmail.com>
2026-07-30 21:44:53 +0300

    Minor changes

--- terraform/environments/dev/providers.tf
+++ terraform/environments/dev/providers.tf
@@ -3,9 +3,9 @@ provider "aws" {
 
   default_tags {
     tags = {
-        Project = "var.project_name"
-        Environment = "var.environment"
-        ManagedBy = "Terraform"
+      Project     = "var.project_name"
+      Environment = "var.environment"
+      ManagedBy   = "Terraform"
     }
   }
 }
