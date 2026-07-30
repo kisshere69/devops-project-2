9f3eb2ee6af8700ed4af3f4c9b72f868b82a77e3
Nikita <abc@gmail.com>
2026-07-30 21:20:59 +0300

    Updated variables.tf

---
+++ terraform/environments/dev/variables.tf
@@ -0,0 +1,13 @@
+variable "aws_region" {
+  description = "AWS Region for the entire Project"
+  type        = string
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
