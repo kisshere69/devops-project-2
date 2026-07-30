829069c520480527476a9f664ea23a4b6c7efe55
Nikita <abc@gmail.com>
2026-07-30 21:34:59 +0300

    Added basic output

---
+++ terraform/environments/dev/outputs.tf
@@ -0,0 +1,11 @@
+output "aws_account_id" {
+  description = "AWS Account ID"
+
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "aws_caller_arn" {
+  description = "Current IAM ARN"
+
+  value = data.aws_caller_identity.current.arn
+}
