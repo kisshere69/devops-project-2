852828f0685088511a59286686582ba303a3dc5a
Nikita <abc@gmail.com>
2026-07-31 09:42:14 +0300

    Added public subnet and availability zone

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -8,4 +8,7 @@ module "vpc" {
   project_name = var.project_name
   environment  = var.environment
   vpc_cidr     = var.vpc_cidr
+
+  public_subnet_a_cidr = var.public_subnet_a_cidr
+  az_a                 = var.az_a
 }
