ddcccd4cce737d11b45ef0e847c62e5b07018103
Nikita <abc@gmail.com>
2026-07-31 09:43:21 +0300

    Added public-subnet-a and az-a

--- terraform/environments/dev/terraform.example.tfvars
+++ terraform/environments/dev/terraform.example.tfvars
@@ -1,4 +1,7 @@
 aws_region   = "value"
 project_name = "value"
 environment  = "value"
-vpc_cidr = "1.2.3.4/67"
+vpc_cidr     = "1.2.3.4/67"
+
+public_subnet_a_cidr = "3.2.1.0/69"
+az_a                 = "value"
--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -16,3 +16,13 @@ variable "vpc_cidr" {
   description = "CIDR for the VPC"
   type        = string
 }
+
+variable "public_subnet_a_cidr" {
+  description = "CIDR block for Public Subnet A"
+  type        = string
+}
+
+variable "az_a" {
+  description = "Primary Availability Zone"
+  type        = string
+}
