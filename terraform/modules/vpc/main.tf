57233074c1ad6b2d6f90cf9c7ad957a7d0205969
Nikita <abc@gmail.com>
2026-07-31 15:02:57 +0300

    Associated route table with the public subnet

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -50,3 +50,8 @@ resource "aws_route" "public_internet_access" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.this.id
 }
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
