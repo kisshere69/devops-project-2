dc9a19db79fe789110eb3619a2e021274fa378af
Nikita <abc@gmail.com>
2026-07-31 13:43:27 +0300

    Added Internet Gateway resource

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -23,3 +23,13 @@ resource "aws_subnet" "public_a" {
     Type        = "Public"
   }
 }
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-igw"
+    Project     = var.project_name
+    Environment = var.environment
+  }
+}
