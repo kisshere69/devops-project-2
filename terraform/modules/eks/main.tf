779adbee5925fe2c10d3b727057cf0442849090d
Nikita <abc@gmail.com>
2026-08-03 12:14:51 +0300

    Created a resource for EKS Node Group EC2 instances creation & configuration

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,3 +1,5 @@
+# EKS Cluster IAM Role
+
 resource "aws_iam_role" "eks_cluster" {
 
   name = "${var.project_name}-${var.environment}-eks-cluster-role"
@@ -27,6 +29,8 @@ resource "aws_iam_role" "eks_cluster" {
   }
 }
 
+# IAM Role for EKS Managed Nodes (EC2 instances)
+
 resource "aws_iam_role" "eks_nodes" {
   name = "${var.project_name}-${var.environment}-eks-node-role"
 
@@ -53,6 +57,8 @@ resource "aws_iam_role" "eks_nodes" {
   }
 }
 
+# IAM Role Policy Attachments
+
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
 
   role = aws_iam_role.eks_cluster.name
@@ -74,6 +80,8 @@ resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
   role       = aws_iam_role.eks_nodes.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
+
+# EKS Cluster
 
 resource "aws_eks_cluster" "this" {
 
@@ -99,4 +107,46 @@ resource "aws_eks_cluster" "this" {
     Project     = var.project_name
     Environment = var.environment
   }
+}
+
+# EKS Managed Node Group. Creates and configures EC2 instances that will join the EKS cluster.
+
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = var.node_group_name
+  node_role_arn   = aws_iam_role.eks_nodes.arn
+
+  subnet_ids = var.private_subnet_ids
+
+  version        = var.cluster_version
+  instance_types = var.node_instance_types
+  capacity_type  = var.node_capacity_type
+  disk_size      = var.node_disk_size
+
+  scaling_config {
+    desired_size = var.node_desired_size
+    min_size     = var.node_min_size
+    max_size     = var.node_max_size
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  labels = {
+    Environment = var.environment
+    NodeGroup   = var.node_group_name
+  }
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-${var.node_group_name}"
+    Project     = var.project_name
+    Environment = var.environment
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_ecr_pull_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy
+  ]
 }
