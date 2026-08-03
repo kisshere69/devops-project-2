3f198dbe68c53485cd3e0a57eb245dce63b67547
Nikita <abc@gmail.com>
2026-08-03 12:25:47 +0300

    Minor changes. Formated several lines

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -36,9 +36,9 @@ module "eks" {
   cluster_name    = var.project_name
   cluster_version = var.cluster_version
 
-  node_group_name    = var.node_group_name
+  node_group_name     = var.node_group_name
   node_instance_types = var.node_instance_types
-  node_capacity_type = var.node_capacity_type
+  node_capacity_type  = var.node_capacity_type
 
   node_desired_size = var.node_desired_size
   node_min_size     = var.node_min_size
