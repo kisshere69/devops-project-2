68399b285e4fcd854c9dde56843f978dd8e79141
Nikita <abc@gmail.com>
2026-08-07 20:38:22 +0300

    Added extra variables, and refactored code

--- terraform/modules/github-oidc/variables.tf
+++ terraform/modules/github-oidc/variables.tf
@@ -1,12 +1,14 @@
-variable "github_repository" {
-  description = "GitHub repository allowed to assume the IAM role"
+# AWS common variables
+
+variable "aws_region" {
+  description = "AWS region used by the project"
   type        = string
+  default     = "eu-central-1"
 }
 
-variable "github_branch" {
-  description = "GitHub branch allowed to assume the IAM role"
+variable "aws_account_id" {
+  description = "AWS account ID"
   type        = string
-  default     = "main"
 }
 
 variable "project_name" {
@@ -19,6 +21,8 @@ variable "environment" {
   type        = string
 }
 
+# EKS variables
+
 variable "eks_cluster_arn" {
   description = "ARN of the EKS cluster"
   type        = string
@@ -29,6 +33,8 @@ variable "eks_cluster_name" {
   type        = string
 }
 
+# GitHub variables
+
 variable "github_oidc_subject" {
   description = "Exact GitHub OIDC subject allowed to assume the IAM role"
   type        = string
@@ -37,4 +43,20 @@ variable "github_oidc_subject" {
 variable "github_oidc_provider_arn" {
   description = "ARN of the shared GitHub Actions OIDC provider"
   type        = string
+}
+
+variable "github_repository" {
+  description = "GitHub repository allowed to assume the IAM role"
+  type        = string
+}
+
+variable "repository_name" {
+  description = "Amazon ECR repository name"
+  type        = string
+}
+
+variable "github_branch" {
+  description = "GitHub branch allowed to assume the IAM role"
+  type        = string
+  default     = "main"
 }
